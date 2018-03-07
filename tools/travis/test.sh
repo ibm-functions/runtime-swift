8491956227cc20bb21463aad30c07d036464f02e
Carlos Santana <csantanapr@apache.org>
2018-03-06 20:02:29 -0500

    remove wsk cli test after deploy

--- tools/travis/test.sh
+++ tools/travis/test.sh
@@ -7,16 +7,8 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/../.."
 WHISKDIR="$ROOTDIR/../openwhisk"
 
-#Deployment
-WHISK_CLI="${WHISKDIR}/bin/wsk -i"
-
-# Run a simple action using the kind
-${WHISK_CLI} action update echoSwift40 ${ROOTDIR}/tests/dat/actions/echo/main.swift --kind "swift:4.0"
-${WHISK_CLI} action invoke echoSwift40 -b -p kind swift40
-${WHISK_CLI} action update echoSwift41 ${ROOTDIR}/tests/dat/actions/echo/main.swift --kind "swift:4.1"
-${WHISK_CLI} action invoke echoSwift41 -b -p kind swift41
-
 export OPENWHISK_HOME=$WHISKDIR
+
 cd ${ROOTDIR}
 TERM=dumb ./gradlew :tests:checkScalafmtAll
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
