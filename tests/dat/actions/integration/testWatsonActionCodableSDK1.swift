0d013be43b0ac515bb597f91f04f9478edf34490
Carlos Santana <csantanapr@apache.org>
2019-01-07 17:14:17 -0500

    moved to based

---
+++ tests/dat/actions/integration/testWatsonActionCodableSDK1.swift
@@ -0,0 +1,31 @@
+print("debug1")
+import Foundation
+import LanguageTranslatorV3
+
+
+struct Input: Codable {
+    let username: String
+    let password: String
+    let url: String?
+}
+struct Output: Codable {
+    let translation: String
+}
+func main(param: Input, completion: @escaping (Output?, Error?) -> Void) -> Void {
+    let languageTranslator = LanguageTranslator(username: param.username , password: param.password, version: "2018-09-16")
+    languageTranslator.translate(text: ["Hello"], source: "en", target: "es") { (response, error) in
+        if let error = error {
+            print(error)
+            return
+        }
+        guard let translation = response?.result else {
+            print("missing response")
+            return
+        }
+        print(translation)
+        let result = Output(translation: (translation.translations.first?.translationOutput)!)
+        print(result)
+        completion(result, nil)
+    }
+
+}
