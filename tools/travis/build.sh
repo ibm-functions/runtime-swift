1ee1594b837d749442d9f9b94badd2b4c9734e7f
Luke-Roy-IBM <83647952+Luke-Roy-IBM@users.noreply.github.com>
2022-03-04 12:22:32 +0100

    remove unnecessary docker instalation (#92)

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -30,11 +30,6 @@ docker tag openwhisk/nodejs6action:nightly nodejs6action
 
 TERM=dumb ./gradlew install
 
-# install new version docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-docker version
-
 # Build runtime
 cd $ROOTDIR
 TERM=dumb ./gradlew \
