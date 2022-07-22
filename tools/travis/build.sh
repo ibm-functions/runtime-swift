048af0ef45c7a6846c49192f50e3ab44eed6b584
Luke Roy <luke.roy@ibm.com>
2022-07-22 13:20:12 +0200

    Set Controller and Invoker to ibmfunctions image

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -21,10 +21,10 @@ fi
 cd $WHISKDIR
 
 #pull down images
-docker pull openwhisk/controller:ecb2a98
-docker tag openwhisk/controller:ecb2a98 ${IMAGE_PREFIX}/controller
-docker pull openwhisk/invoker:ecb2a98
-docker tag openwhisk/invoker:ecb2a98 ${IMAGE_PREFIX}/invoker
+docker pull ibmfunctions/controller:nightly
+docker tag ibmfunctions/controller:nightly ${IMAGE_PREFIX}/controller
+docker pull ibmfunctions/invoker:nightly
+docker tag ibmfunctions/invoker:nightly ${IMAGE_PREFIX}/invoker
 docker pull openwhisk/nodejs6action:nightly
 docker tag openwhisk/nodejs6action:nightly nodejs6action
 
